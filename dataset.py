97e9c02a7cad22d0da623b9cb4a9e334405728d7
BK Bolisetty <bk@squarefruitlabs.com>
2016-02-06 18:06:02 -0600

    Plotting works.

--- dataset.py
+++ dataset.py
@@ -7,7 +7,10 @@ from pybrain.tools.shortcuts import buildNetwork as bn
 from pybrain.supervised.trainers import BackpropTrainer as bt
 from pybrain.structure.modules import SoftmaxLayer as sl
 
-from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
+#from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
+#from pylab import *
+import matplotlib
+import matplotlib.pylab as mp
 from scipy import diag, arange, meshgrid, where
 from numpy.random import multivariate_normal
 
@@ -27,13 +30,13 @@ trndata._convertToOneOfMany()
 tstdata._convertToOneOfMany()
 
 print "Number of patterns: ", len(trndata)
-print "Input and output dimensions: ", trndata,indim, trndata.outdim
+print "Input and output dimensions: ", trndata.indim, trndata.outdim
 print "First sample(input, target, class): "
 print trndata['input'][0], trndata['target'][0], trndata['class'][0]
 
-fnn = bn(trndata,indim, 5, trndata.outdim, outclass=sl)
+fnn = bn(trndata.indim, 5, trndata.outdim, outclass=sl)
 
-trainer = BackpropTrainer( fnn, dataset=trndata, momentum=0.1, verbose=True, weightdecay=0.01)
+trainer = bt( fnn, dataset=trndata, momentum=0.1, verbose=True, weightdecay=0.01)
 
 ticks = arange(-3., 6., 0.2)
 X, Y = meshgrid(ticks, ticks)
@@ -44,26 +47,26 @@ for i in xrange(X.size):
 griddata._convertToOneOfMany() 
 
 for i in range(20):
-	trainer.trainEposhs(1)
+	trainer.trainEpochs(1)
 	trnresult = pe(trainer.testOnClassData(), trndata['class'])
 	tstresult = pe(trainer.testOnClassData(dataset=tstdata), tstdata['class'])
 
 	print "epoch: %4d" % trainer.totalepochs, "  train error: %5.2f%%" % trnresult, "  test error: %5.2f%%" % tstresult
 
-	out = fnn.activateOnDataSet(griddata)
+	out = fnn.activateOnDataset(griddata)
 	out = out.argmax(axis=1)
 	out = out.reshape(X.shape)
-	figure(1)
-	ioff()
-	clf()
-	hold(True)
+	mp.figure(1)
+	mp.ioff()
+	mp.clf()
+	mp.hold(True)
 	for c in [0, 1, 2]: 
 		here, _ = where(tstdata['class']==c)
-		plot(tstdata['input'][here, 0], tstdata['input'][here,1],'o')
+		mp.plot(tstdata['input'][here, 0], tstdata['input'][here,1],'o')
 	if out.max()!=out.min():
-		contourf(X,Y,out)
-	ion()
-	draw()
+		mp.contourf(X,Y,out)
+	mp.ion()
+	mp.draw()
 
-ioff()
-show()
+mp.ioff()
+mp.show()
